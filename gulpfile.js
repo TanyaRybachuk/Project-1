2c83d84630519ab6ec863af324ee172df313312a
TanyaRybachuk <bobcat@Bobs-MacBook-Air.local>
2015-08-07 15:50:01 +0300

    new
    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('connect', function() {
   connect.server({
@@ -15,12 +18,30 @@ gulp.task('html', function () {
 gulp.task('watch', function () {
   gulp.watch(['./*.html'], ['html']);
    gulp.watch(['./*.css'], ['css']);
+   gulp.watch('./sass/**/*.scss', ['sass']);
 });
  
 gulp.task('css', function () {
   gulp.src('./*.css')
     .pipe(connect.reload());
 });
+
  
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+ 
+gulp.task('default', function () {
+    return gulp.src('src/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 22 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css/min/'));
+});
 
 gulp.task('default', ['connect', 'watch']);
+ 
